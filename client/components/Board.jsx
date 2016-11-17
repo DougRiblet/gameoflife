e459d435285f5fc5c1c390cdb9b4e5e0cb0d1d3f
Doug Riblet <smashzen85@gmail.com>
2016-11-17 16:20:01 -0600

    redraw board function, first stab

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -3,24 +3,51 @@ import React from 'react'
 export default class Board extends React.Component {
   constructor (props) {
     super(props)
-    this.grid = Array.from({length: 40}, () => Array.from({length: 80}, () => Math.random() < 0.2))
+    this.state = {
+      grid: Array.from({length: 40}, () => Array.from({length: 80}, () => Math.random() < 0.2))
+    }
+  }
+
+  redrawBoard () {
+    let newgrid = this.grid.map((row, y) => {
+      return row.map((dot, x) => {
+        let nearby = []
+        nearby.push(this.grid[(y - 1) % 40][(x - 1) % 80])
+        nearby.push(this.grid[(y - 1) % 40][x])
+        nearby.push(this.grid[(y - 1) % 40][(x + 1) % 80])
+        nearby.push(this.grid[y][(x - 1) % 80])
+        nearby.push(this.grid[y][(x + 1) % 80])
+        nearby.push(this.grid[(y + 1) % 40][(x - 1) % 80])
+        nearby.push(this.grid[(y + 1) % 40][x])
+        nearby.push(this.grid[(y + 1) % 40][(x + 1) % 80])
+        let nearcount = nearby.filter(i => true).length
+        if (nearcount === 3 || (dot && nearcount === 2)) {
+          return true
+        } else {
+          return false
+        }
+      })
+    })
+    this.setState({grid: newgrid})
   }
 
   render () {
     return (
-      <svg width='800' height='400'>
-        <rect width='800' height='400' fill='papayawhip' />
-        {
-          this.grid.map((row, y) => {
-            return row.map((dot, x) => {
-              let oxo = 'on' + String(dot)
-              return (
-                <circle className={oxo} cx={x * 10 + 5} cy={y * 10 + 5} r='4' />
-              )
+      <div>
+        <svg width='802' height='402'>
+          <rect width='802' height='402' fill='papayawhip' />
+          {
+            this.state.grid.map((row, y) => {
+              return row.map((dot, x) => {
+                let oxo = 'on' + String(dot)
+                return (
+                  <circle className={oxo} cx={x * 10 + 6} cy={y * 10 + 6} r='4' />
+                )
+              })
             })
-          })
-        }
-      </svg>
+          }
+        </svg>
+      </div>
     )
   }
 
