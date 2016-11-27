d4f76ecad866f9e3295342d5b687a25680b68d0e
Doug Riblet <smashzen85@gmail.com>
2016-11-27 00:01:13 -0600

    Tweak history text, link components

--- client/components/History.jsx
+++ client/components/History.jsx
@@ -9,7 +9,7 @@ export default class History extends React.Component {
   render () {
     return (
       <div>
-        <p>British mathematician John Conway invented the Game of Life in 1970. The game involves tracking changing patterns on a two-dimensional grid according to a simple set of rules. Early enthusiasts tracked changes using pencil and paper or chess boards, but computers soon became the preferred tool.</p>
+        <p>British mathematician John Conway invented the Game of Life in 1970. The game involves tracking patterns on a two-dimensional grid. The patterns change from round to round according to a simple set of rules. Early enthusiasts tracked changes using pencil and paper or chess boards, but computers soon became the preferred tool.</p>
         <p>Each cell in the grid has one of two states: on or off, living or dead. You can set up an initial pattern or have it generated randomly. With each round or "generation", a cell's state is determined by interaction with its eight neighboring cells. The rules:</p>
         <ul>
           <li>A living cell with 2 or 3 live neighbors stays on ("Survival").</li>
