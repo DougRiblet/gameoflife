bb3e34787535417c00b46d5dcb13e740c7f60914
Doug Riblet <smashzen85@gmail.com>
2016-11-29 17:48:18 -0600

    fix button typo

--- client/components/Controls.jsx
+++ client/components/Controls.jsx
@@ -12,7 +12,7 @@ export default class Controls extends React.Component {
         <button onClick={this.props.generateRandomBoard} >Random</button>
         <button onClick={this.props.clearBoard} >Clear</button>
         <button onClick={this.props.generateGGG} >GGG</button>
-        <button onClick={this.props.generateGGG} >Acorn</button>
+        <button onClick={this.props.generateAcorn} >Acorn</button>
         <button onClick={this.props.stepForward} >Step</button>
         <button onClick={this.props.startGens} >Start</button>
         <button onClick={this.props.pauseGens} >Pause</button>
